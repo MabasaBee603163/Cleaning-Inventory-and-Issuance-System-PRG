58c2bb4b7442f8358eb85cdf8cfe5ddad3168d41
Reneilwe Makete <reneilwemakete@gmail.com>
2026-07-22 09:29:45 +0200

    Update MaterialDAO.java

--- src/za/ac/belgiumcampus/cleaninginventory/dao/MaterialDAO.java
+++ src/za/ac/belgiumcampus/cleaninginventory/dao/MaterialDAO.java
@@ -1,4 +1,17 @@
 package za.ac.belgiumcampus.cleaninginventory.dao;
 
+import za.ac.belgiumcampus.cleaninginventory.model.Material;
+import java.util.List;
+
 public interface MaterialDAO {
+    boolean addMaterial(Material material);
+    boolean updateMaterial(Material material);
+    boolean deleteMaterial(long materialId);
+    Material getMaterialById(long materialId);
+    List<Material> getAllMaterials();
+    List<Material> searchMaterials(String keyword);
+    List<Material> getLowStockMaterials();
+    boolean updateStock(long materialId, int newQuantity);
+    int getTotalMaterials();
+    int getLowStockCount();
 }
