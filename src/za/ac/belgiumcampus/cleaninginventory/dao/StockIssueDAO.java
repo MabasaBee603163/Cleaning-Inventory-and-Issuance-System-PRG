71d8475a9e5a00b310e70372a5058135c68511cc
Reneilwe Makete <reneilwemakete@gmail.com>
2026-07-22 09:32:44 +0200

    Update StockIssueDAO.java

--- src/za/ac/belgiumcampus/cleaninginventory/dao/StockIssueDAO.java
+++ src/za/ac/belgiumcampus/cleaninginventory/dao/StockIssueDAO.java
@@ -1,4 +1,15 @@
 package za.ac.belgiumcampus.cleaninginventory.dao;
 
+import za.ac.belgiumcampus.cleaninginventory.model.StockIssueHeader;
+import za.ac.belgiumcampus.cleaninginventory.model.StockIssueItem;
+import java.util.List;
+
 public interface StockIssueDAO {
+    boolean issueMaterials(long cleanerId, long userId, List<StockIssueItem> items, String remarks);
+    List<StockIssueHeader> getAllIssuances();
+    List<StockIssueHeader> getRecentIssuances(int limit);
+    StockIssueHeader getIssuanceById(long issueId);
+    List<StockIssueItem> getIssueItems(long issueId);
+    boolean deleteIssuance(long issueId);
+    int getTotalIssuances();
 }
