b0fb94585990f6edcc7104f9e5ca68c3566adb1f
Reneilwe Makete <reneilwemakete@gmail.com>
2026-07-22 09:32:00 +0200

    Update CleanerDAO.java

--- src/za/ac/belgiumcampus/cleaninginventory/dao/CleanerDAO.java
+++ src/za/ac/belgiumcampus/cleaninginventory/dao/CleanerDAO.java
@@ -1,4 +1,14 @@
 package za.ac.belgiumcampus.cleaninginventory.dao;
 
+import za.ac.belgiumcampus.cleaninginventory.model.Cleaner;
+import java.util.List;
+
 public interface CleanerDAO {
+    boolean addCleaner(Cleaner cleaner);
+    boolean updateCleaner(Cleaner cleaner);
+    boolean deleteCleaner(long cleanerId);
+    Cleaner getCleanerById(long cleanerId);
+    List<Cleaner> getAllCleaners();
+    List<Cleaner> searchCleaners(String keyword);
+    int getTotalCleaners();
 }
