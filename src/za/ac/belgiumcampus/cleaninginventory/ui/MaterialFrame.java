e821cee453a5bacf2a96ff051451ca2cafb066de
Reneilwe Makete <reneilwemakete@gmail.com>
2026-07-22 09:15:11 +0200

    Update MaterialFrame.java

--- src/za/ac/belgiumcampus/cleaninginventory/ui/MaterialFrame.java
+++ src/za/ac/belgiumcampus/cleaninginventory/ui/MaterialFrame.java
@@ -1,4 +1,4 @@
-package za.ac.belgiumcampus.cleaninginventory.view;
+package za.ac.belgiumcampus.cleaninginventory.ui;
 
 import za.ac.belgiumcampus.cleaninginventory.model.Material;
 import za.ac.belgiumcampus.cleaninginventory.model.Supplier;
