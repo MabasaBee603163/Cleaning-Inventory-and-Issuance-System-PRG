29a20744246d9109d19f08ac1c58755cb767e805
Reneilwe Makete <reneilwemakete@gmail.com>
2026-07-22 09:30:35 +0200

    Update SupplierDAO.java

--- src/za/ac/belgiumcampus/cleaninginventory/dao/SupplierDAO.java
+++ src/za/ac/belgiumcampus/cleaninginventory/dao/SupplierDAO.java
@@ -1,4 +1,14 @@
 package za.ac.belgiumcampus.cleaninginventory.dao;
 
+import za.ac.belgiumcampus.cleaninginventory.model.Supplier;
+import java.util.List;
+
 public interface SupplierDAO {
+    boolean addSupplier(Supplier supplier);
+    boolean updateSupplier(Supplier supplier);
+    boolean deleteSupplier(long supplierId);
+    Supplier getSupplierById(long supplierId);
+    List<Supplier> getAllSuppliers();
+    List<Supplier> searchSuppliers(String keyword);
+    int getTotalSuppliers();
 }
