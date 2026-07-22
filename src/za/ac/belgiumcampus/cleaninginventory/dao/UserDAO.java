eda32043439e4b6553245e873d3b40cddf46d922
Reneilwe Makete <reneilwemakete@gmail.com>
2026-07-22 09:29:09 +0200

    Update UserDAO.java

--- src/za/ac/belgiumcampus/cleaninginventory/dao/UserDAO.java
+++ src/za/ac/belgiumcampus/cleaninginventory/dao/UserDAO.java
@@ -1,4 +1,16 @@
 package za.ac.belgiumcampus.cleaninginventory.dao;
 
+import za.ac.belgiumcampus.cleaninginventory.model.User;
+import java.util.List;
+
 public interface UserDAO {
+    User authenticateUser(String username, String password);
+    boolean addUser(User user);
+    User getUserById(long userId);
+    User getUserByUsername(String username);
+    List<User> getAllUsers();
+    boolean updateUser(User user);
+    boolean deleteUser(long userId);
+    boolean usernameExists(String username);
+    boolean emailExists(String email);
 }
